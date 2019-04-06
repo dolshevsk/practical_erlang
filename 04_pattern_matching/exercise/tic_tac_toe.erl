20a82e0341e66e9997f1e337a7e76703c2098ddd
dolshevsk <olshevskiydk@gmail.com>
2019-04-06 15:26:46 +0300

    lesson05:done

--- 04_pattern_matching/exercise/tic_tac_toe.erl
+++ 04_pattern_matching/exercise/tic_tac_toe.erl
@@ -10,8 +10,54 @@ new_game() ->
 
 
 win(GameState) ->
-    no_win.
+    case GameState of
+        {{Y, Y, Y},
+        {_, _, _},
+        {_, _, _}} when Y =/= f -> {win,Y};
+        
+        {{Y, _, _},
+        {Y, _, _},
+        {Y, _, _}} when Y =/= f -> {win,Y};
+
+        {{_, _, _},
+        {Y, Y, Y},
+        {_, _, _}} when Y =/= f -> {win,Y};
+
+        {{_, _, _},
+        {_, _, _},
+        {Y, Y, Y}} when Y =/= f -> {win,Y};
+
+        {{_, Y, _},
+        {_, Y, _},
+        {_, Y, _}} when Y =/= f -> {win,Y};
+
+        {{_, _, Y},
+        {_, _, Y},
+        {_, _, Y}} when Y =/= f -> {win,Y};
+
+        {{Y, _, _},
+        {_, Y, _},
+        {_, _, Y}} when Y =/= f -> {win,Y};
+
+        {{_, _, Y},
+        {_, Y, _},
+        {Y, _, _}} when Y =/= f -> {win,Y};
+
+        _ -> no_win
+        end.
+
 
 
 move(Cell, Player, GameState) ->
-    {error, invalid_move}.
+        case {Cell, GameState} of
+            {1, {{f,Y2,Y3},V2,V3}} when Player == x orelse Player == o -> {ok, {{Player,Y2,Y3},V2,V3}};
+            {2, {{Y1,f,Y3},V2,V3}} when Player == x orelse Player == o -> {ok, {{Y1,Player,Y3},V2,V3}};
+            {3, {{Y1,Y2,f},V2,V3}} when Player == x orelse Player == o -> {ok, {{Y1,Y2,Player},V2,V3}};
+            {4, {V1,{f,Y2,Y3},V3}} when Player == x orelse Player == o -> {ok, {V1,{Player,Y2,Y3},V3}};
+            {5, {V1,{Y1,f,Y3},V3}} when Player == x orelse Player == o -> {ok, {V1,{Y1,Player,Y3},V3}};
+            {6, {V1,{Y1,Y2,f},V3}} when Player == x orelse Player == o -> {ok, {V1,{Y1,Y2,Player},V3}};
+            {7, {V1,V2,{f,Y2,Y3}}} when Player == x orelse Player == o -> {ok, {V1,V2,{Player,Y2,Y3}}};
+            {8, {V1,V2,{Y1,f,Y3}}} when Player == x orelse Player == o -> {ok, {V1,V2,{Y1,Player,Y3}}};
+            {9, {V1,V2,{Y1,Y2,f}}} when Player == x orelse Player == o -> {ok, {V1,V2,{Y1,Y2,Player}}};
+            _ -> {error,invalid_move}
+        end.
--- 04_pattern_matching/exercise_hard/fizz_buzz.erl
+++ 04_pattern_matching/exercise_hard/fizz_buzz.erl
@@ -1,2 +1,9 @@
 %% Реализовать Fizz Buzz
 %% https://habr.com/ru/post/298134/
+
+-module (fizz_buzz).
+
+-export ([fizz_buzz/1]).
+
+fizz_buzz(List) ->
+    lists:foldr(fun(X,Acc) when X rem 3==0, X rem 5==0, X =/=0 -> ["FizzBuzz"|Acc]; (X,Acc) when X rem 5==0, X =/=0 -> ["Buzz"|Acc]; (X,Acc) when X rem 3==0, X =/=0 -> ["Fizz"|Acc]; (X,Acc)->[X|Acc] end, [], List).
