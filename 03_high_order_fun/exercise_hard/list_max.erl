77b0c999bc017e18c187ef7cf62f5c7debf7733f
dolshevsk <olshevskiydk@gmail.com>
2019-04-06 13:03:47 +0300

    list_max:done

--- 03_high_order_fun/exercise_hard/list_max.erl
+++ 03_high_order_fun/exercise_hard/list_max.erl
@@ -1,1 +1,8 @@
 %% Реализовать функцию, которая принимает список, и возвращает максимальный элемент этого списка
+-module(list_max).
+
+-export([max/1]).
+
+max(List) ->
+[H|T] = List,
+lists:foldl(fun(A,B) when A>=B-> A; (_,B) ->B end, H,T).
