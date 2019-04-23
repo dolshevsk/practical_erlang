a055a25beea378a47046d5ec2cc30d9e684404d4
dolshevsk <olshevskiydk@gmail.com>
2019-04-23 17:06:21 +0300

    task07:refractor code

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -4,11 +4,14 @@
 
 parse(Str, Data) when is_binary(Str) ->
     Split = binary:split(Str, [<<"{{">>, <<"}}">>], [global]),
-    Reply = lists:map(fun(Word) ->   
-                                    case maps:find(Word, Data) of
-                                        error -> Word;
-                                        {ok, Value} when is_integer(Value) -> integer_to_binary(Value);
-                                        {ok, Value} -> Value
-                                    end 
+    Reply = lists:map(fun(Word) ->  case binary:match(Word, <<" ">>) of
+                                        nomatch -> case maps:find(Word, Data) of                            
+                                                        {ok, Value} when is_integer(Value) -> integer_to_binary(Value);
+                                                        {ok, Value} -> Value;
+                                                        error -> ""
+                                                    end;
+                                        _ -> Word
+                                    
+                                end
                         end, Split),
     unicode:characters_to_binary(Reply).
