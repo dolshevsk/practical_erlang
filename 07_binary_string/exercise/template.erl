0ea6766e057b009fa4e4cba545644b3ac2d265ea
dolshevsk <olshevskiydk@gmail.com>
2019-04-23 16:49:13 +0300

    task07:finally done

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -3,4 +3,12 @@
 -export([parse/2]).
 
 parse(Str, Data) when is_binary(Str) ->
-    Str.
+    Split = binary:split(Str, [<<"{{">>, <<"}}">>], [global]),
+    Reply = lists:map(fun(Word) ->   
+                                    case maps:find(Word, Data) of
+                                        error -> Word;
+                                        {ok, Value} when is_integer(Value) -> integer_to_binary(Value);
+                                        {ok, Value} -> Value
+                                    end 
+                        end, Split),
+    unicode:characters_to_binary(Reply).
