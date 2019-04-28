0cfe4ce524553be2b93d9efa5714c9d1f8a99de8
dolshevsk <olshevskiydk@gmail.com>
2019-04-29 01:17:38 +0300

    task16:almost done

--- 16_sockets/exercise/src/mcache_server.erl
+++ 16_sockets/exercise/src/mcache_server.erl
@@ -2,7 +2,6 @@
 
 -export([start_link/0, start_acceptor/2, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(state, {storage=maps:new()}).
 
 start_link() ->
     gen_server:start_link({local, server}, ?MODULE, [], []).
@@ -13,7 +12,7 @@ init([]) ->
     io:format("Starting server at port ~p~n", [Port]),
     {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {active, false}, {reuseaddr, true}, {packet, line}]),
     [spawn(?MODULE, start_acceptor, [ID, ListenSocket]) || ID <- lists:seq(1, NumAcceptors)],
-    {ok, #state{}}.
+    {ok, #{}}.
 
 start_acceptor(ID, ListenSocket) ->
     io:format("Acceptor: handle_connection ~p ~p~n", [self(), ID]),
@@ -26,9 +25,8 @@ handle_connection(ID, ListenSocket, AcceptSocket) ->
     case gen_tcp:recv(AcceptSocket, 0) of
         {ok, Msg} ->
             MsgBin = binary:part(Msg, 0, byte_size(Msg) - 2),
-            Reply = call(parse(MsgBin)),
-            BinId = list_to_binary(integer_to_list(ID)),
-            gen_tcp:send(AcceptSocket, <<"From", BinId/binary, ":" ,Reply/binary, "\r\n">>),
+            Reply = call_serv(parse(MsgBin)),
+            gen_tcp:send(AcceptSocket, <<Reply/binary, "\r\n">>),
             handle_connection(ID, ListenSocket, AcceptSocket);
         {error, closed} ->
             io:format("connected closed"),
@@ -54,7 +52,7 @@ params(Tail) ->
     [Key, Value] -> {Key, Value}
     end.
 
-call(Request) ->
+call_serv(Request) ->
     gen_server:call(server, Request).
 
 
