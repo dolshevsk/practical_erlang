420945cfb79f19015dcbfdea59b4b9175d671a43
dolshevsk <olshevskiydk@gmail.com>
2019-05-01 02:55:33 +0300

    task11:done

--- 11_link_monitor/exercise/main.erl
+++ 11_link_monitor/exercise/main.erl
@@ -3,5 +3,57 @@
 -export([parse/1]).
 
 
+-record(state, {
+    statistic = #{},
+    errors = #{}
+}).
+
 parse(Files) ->
-    ok.
+    Workers = lists:foldl(
+    fun(File, Acc) ->
+        Pid = spawn(worker, parser, [File, self()]),
+        Ref = erlang:monitor(process, Pid),
+        Worker = {Pid, Ref},
+        Acc#{Worker => File}
+    end,
+    #{},
+    Files),
+    loop(Workers, #state{}).
+
+
+loop(Workers, #state{statistic=Stat, errors=Errors}=State) ->
+    case map_size(Workers) =< 0 of
+        true -> {Stat, Errors};
+        false -> 
+            receive
+                {result, Data} -> 
+                    NewState = aggr(Data, State),
+                    loop(Workers, NewState);
+                {'DOWN', Ref, process, Pid, Reason} ->
+                    Worker = {Pid, Ref},
+                    NewWorkers = maps:remove(Worker, Workers),
+                    case Reason of
+                        normal -> loop(NewWorkers, State);
+                        _ ->
+                            File = maps:get(Worker, Workers),
+                            NewState = State#state{errors = maps:put(File, Reason, Errors)},
+                            loop(NewWorkers, NewState)
+                    end
+            after
+                1000 -> {error, no_reply}
+            end
+    end.
+
+
+aggr(Data, #state{statistic=Stat} = State) ->
+    NewStatistic = maps:fold(
+        fun (Name, Count, Acc) ->
+            case maps:find(Name, Stat) of
+                {ok, OldCount} -> Acc#{Name => OldCount + Count};
+                error -> Acc#{Name => Count}
+            end
+        end,
+    Stat,
+    Data
+    ),
+    State#state{statistic = NewStatistic}.
--- 11_link_monitor/exercise/worker.erl
+++ 11_link_monitor/exercise/worker.erl
@@ -1,1 +1,20 @@
 -module(worker).
+-export([parser/2, parse_strings/1]).
+
+parser(File, Pid) ->
+    {ok, Bin} = file:read_file(File),
+    Strings = binary:split(Bin, <<"\n">>, [global]),
+    ParsedResult = parse_strings(Strings),
+    Pid ! {result, ParsedResult}.
+
+parse_strings(Lines) ->
+    lists:foldl(
+    fun
+        (<<>>, Acc) -> Acc;
+        (Line, Acc) ->
+            [_Id, Name, BinaryQuantity, _Price]= binary:split(Line, <<",">>, [global]),
+            IntegerQuantity = list_to_integer(binary_to_list(BinaryQuantity)),
+            Acc#{Name => IntegerQuantity}
+    end,
+    #{},
+    Lines).
